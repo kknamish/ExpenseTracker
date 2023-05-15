0a6d10e49c26ac3b126472b919e22c1da4b8e912
Namish KUCHAR KAL <namish.kuchar.kal@hexagon.com>
2023-05-15 20:27:09 +0530

    Styling Footer and adding Github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
     <footer>
         <center>
           <p className='quote'>"Who is rich? He that is content. Who is that? Nobody."</p>
-          <h6>Made with React.js by K.K.Namish.</h6>
+          <h6>Made with React.js by <a href='https://www.github.com/kknamish'>K.K.Namish</a>.</h6>
         </center>
     </footer>
   )
